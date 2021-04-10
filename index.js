c9cdf601c32a04313f3749c2ab9e1bb9512eb6a0
omar-faruk02 <tuhin.omar99@gmail.com>
2021-04-09 22:39:29 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,70 @@
+const express = require('express')
+
+const cors=require('cors')
+
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config()
+const port = 5002
+console.log(process.env.DB_PASS)
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gqnwo.mongodb.net/emajonSample?retryWrites=true&w=majority`;
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const products = client.db("emajonSample").collection("emajon");
+
+  app.post('/addProducts',(req,res)=>{
+    products.insertMany(req.body)
+    .then(result=>{
+       res.send(result.insertContent)
+    })
+
+  })
+
+  app.get('/products', (req,res)=>{
+      products.find({})
+      .toArray((err,documents)=>{
+          res.send(documents)
+
+      })
+
+  })
+
+  app.get('/product/:key', (req,res)=>{
+    products.find({key:req.params.key})
+    .toArray((err,documents)=>{
+        res.send(documents[0])
+
+    })
+
+})
+
+app.post('/productCart',(req,res)=>{
+const productCart=req.body
+  products.find({key:{$in : productCart}})
+  .toArray((err,documents)=>{
+    res.send(documents)
+
+})
+  
+})
+ 
+  
+});
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
